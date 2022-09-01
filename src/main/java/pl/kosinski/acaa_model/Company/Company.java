6092715f55624f3a145ea45924f135b543c1a4ee
Ivoriel <tkivor@gmail.com>
2022-09-01 08:31:55 +0200

    Company moved to the ACAA-model library.

---
+++ src/main/java/pl/kosinski/acaa_model/Company/Company.java
@@ -0,0 +1,11 @@
+package pl.kosinski.acaa_model.Company;
+
+import pl.kosinski.acaa_model.Client.Client;
+
+public class Company {
+
+    private Long id;
+    private String name;
+    private Client client;
+
+}

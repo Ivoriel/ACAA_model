1ffb0b139aaac8e53d982c03d22c23deaf1d1412
Ivoriel <tkivor@gmail.com>
2022-09-18 11:19:49 +0200

    Address object added to model.

---
+++ src/main/java/pl/kosinski/acaa_model/Address/Address.java
@@ -0,0 +1,12 @@
+package pl.kosinski.acaa_model.Address;
+
+public class Address {
+
+    private Long id;
+    private String city;
+    private String zipCode;
+    private String street;
+    private String streetNumber;
+    private String additionalIdentifier;
+
+}

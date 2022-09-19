c7a7209f611b7544f2ff6b634a02c76b78a5e1d5
Ivoriel <tkivor@gmail.com>
2022-09-19 09:35:28 +0200

    Address added to Client.

--- src/main/java/pl/kosinski/acaa_model/Client/Client.java
+++ src/main/java/pl/kosinski/acaa_model/Client/Client.java
@@ -1,8 +1,11 @@
 package pl.kosinski.acaa_model.Client;
 
+import pl.kosinski.acaa_model.Address.Address;
+
 public class Client {
 
     private long id;
     private String name;
+    private Address address;
 
 }

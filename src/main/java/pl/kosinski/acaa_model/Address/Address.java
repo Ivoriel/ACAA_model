11769faa3c3cae556a71717eac4bfe9bc48f9aa9
Ivoriel <tkivor@gmail.com>
2022-10-21 01:22:52 +0200

    Address model updated

--- src/main/java/pl/kosinski/acaa_model/Address/Address.java
+++ src/main/java/pl/kosinski/acaa_model/Address/Address.java
@@ -3,10 +3,12 @@ package pl.kosinski.acaa_model.Address;
 public class Address {
 
     private Long id;
-    private String city;
+    private String country;
+    private String municipality;
+    private String region;
     private String zipCode;
     private String street;
-    private String streetNumber;
+    private String buildingNumber;
     private String additionalIdentifier;
 
 }
